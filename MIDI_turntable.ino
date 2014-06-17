677fd5ad5032e168a540b7ec82ab7b0b04bf9e06
Ben Barwise <clacktronics@gmail.com>
2014-06-17 12:37:59 +0100

    errors fixed

--- MIDI_turntable.ino
+++ MIDI_turntable.ino
@@ -11,6 +11,7 @@ int display_brightness = 20 ; // 0-255
 //// Main variables used ///////////////////////////////////
 
 int i,sequence_number,sequence_step,step_value,tens,ones,data_step;
+int count = 1;
 boolean start_delay,counting_delay,next_step,next_sequence,last_sequence,wait_for_high,wait_for_high_2,trigger_sequence;
 unsigned long start_of_delay,debounce,debounce_2;
 
@@ -29,13 +30,8 @@ int midi_mode,midi_trigger;
 int pinmap[8] = {6,2,9,11,7,5,10,12}; // New pinmap reflects timers
 //int pinmap[8] = {2,11,9,3,4,12,10,5}; // Mega Pinmap
 //int pinmap[8] = {2,5,9,11,8,6,10,3}; // Uno Pinmap
-int count =1;
-
 int next_button = 14;
 int last_button = 15;
-
-
-
 
 void setup() {
   
@@ -43,6 +39,9 @@ TCCR4B = TCCR4B & 0b11111000 | 0x03;
 TCCR3B = TCCR3B & 0b11111000 | 0x03;
 TCCR2B = TCCR2B & 0b11111000 | 0x03;
 TCCR1B = TCCR1B & 0b11111000 | 0x03;
+
+
+///////////////////////////////////////////////////////
   
 Serial.begin(baudrate);
 
